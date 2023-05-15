487e6f4abdf844729402eda9fe0d7d045aee11f4
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-05-16 05:14:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ LOGIN_CHANNEL = int(login_channel) if login_channel and id_pattern.search(login_
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mdisk123:Mdisk123@cluster0.hx59djt.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', ")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Ravjggjn")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
